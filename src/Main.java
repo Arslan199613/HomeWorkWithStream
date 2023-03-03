0ed00c6e30f6d34cc3ae4f53e616d54aa2036c9d
Samsung <arslan.zakirov.1996@mail.ru>
2023-03-03 23:33:21 +0300

    Домашняя работа со стримами, c учетом замечаний +

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 import java.util.*;
 import java.util.function.BiConsumer;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Student student1 = new Student("Иван", 18, 1);
+        Student student1 = new Student("Василий", 18, 1);
         Student student2 = new Student("Сергей", 19, 2);
-        Student student3 = new Student("Федор", 18, 1);
+        Student student3 = new Student("Федор", 19, 1);
         Student student4 = new Student("Мирон", 20, 3);
         Student student5 = new Student("Петр1", 22, 4);
+
+        System.out.println("Задача 1. Найти максимум и минимум");
 
         List<Student> students = new ArrayList<>();
         students.add(student1);
@@ -20,11 +20,13 @@ public class Main {
 
         findMinMax(students);
 
+        System.out.println("Задача 2. Вывод количества чётных чисел");
+        List<Integer> integerList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        evenInteger(integerList);
+
     }
-        // Задача 1. Найти минимальное и максимальное значение.
-        // Создал список студентов,вывел самого молодого и самого взрослого.
-        // Положил в BiConsumer.
-    public static void findMinMax(List<Student>students) {
+    public static void findMinMax(List<Student> students) {
         String min = students.stream()
                 .min(Comparator.comparing(Student::getAge))
                 .map(e -> e.getName()).get();
@@ -35,23 +37,18 @@ public class Main {
                 .map(e -> e.getName()).get();
         System.out.println(max);
 
-        BiConsumer<String,String> minMaxConsumer = (a, b) -> System.out.println("Самый молодой студент- " + a + ", самый взрослый студент- " + b);
+        BiConsumer<String, String> minMaxConsumer = (a, b) -> System.out.println("Самый молодой студент- " + a + ", самый взрослый студент- " + b);
 
         if (minMaxConsumer == null) {
             minMaxConsumer.accept(null, null);
         } else {
             minMaxConsumer.accept(min, max);
         }
-
-        //Задача 2. Вывел четные числа исвользуя стримы
-
-        List<Integer> integerList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-
+    }
+    public static void evenInteger(List<Integer> integerList) {
         long count = integerList.stream()
                 .filter(e -> e % 2 == 0)
                 .count();
         System.out.println(count);
-
-
     }
 }
